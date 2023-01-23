d9b4a825f2f66e7dcc21e38f69808dfd51762ae3
DanDaMan23 <danlawrcons.gmail.com>
2023-01-22 22:01:12 -0600

    Created confirm modal

---
+++ src/features/user_registration_system/modals/confirm.modal.tsx
@@ -0,0 +1,32 @@
+import { FC } from "react"
+import { Modal, Button } from "react-bootstrap"
+
+interface IConfirmModal {
+  id: string
+  show: boolean
+  onSubmit: () => void
+  onClose: () => void
+}
+
+const ConfirmModal: FC<IConfirmModal> = ({ id, show, onClose, onSubmit }) => {
+  return (
+    <>
+      <Modal show={show} onHide={onClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Confirm Registration</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Are your provided information correct?</Modal.Body>
+        <Modal.Footer>
+          <Button variant='secondary' onClick={onClose}>
+            No
+          </Button>
+          <Button variant='primary' onClick={onSubmit}>
+            Yes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  )
+}
+
+export default ConfirmModal
