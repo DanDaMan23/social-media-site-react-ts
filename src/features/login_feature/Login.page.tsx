a64a1da6c92685c54dc8ca20d9d03dc466ebc4dc
DanDaMan23 <danlawrcons.gmail.com>
2023-01-25 21:01:56 -0600

    Link button to register page created

--- src/features/login_feature/Login.page.tsx
+++ src/features/login_feature/Login.page.tsx
@@ -1,10 +1,24 @@
 import { FC } from "react"
+import { Button } from "react-bootstrap"
+import { useNavigate } from "react-router-dom"
 import LoginForm from "./Login.form"
 
 const LoginPage: FC = () => {
+  const navigate = useNavigate()
+
   return (
     <>
       <LoginForm />
+      <hr />
+      <Button
+        variant='success'
+        style={{ width: "100%" }}
+        onClick={() => {
+          navigate("/register")
+        }}
+      >
+        Create an account
+      </Button>
     </>
   )
 }
