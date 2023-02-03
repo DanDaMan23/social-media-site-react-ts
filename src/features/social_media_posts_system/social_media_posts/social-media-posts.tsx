64e6846441fa4ff94eeeb3bdb2cbcbd8827f9af9
DanDaMan23 <danlawrcons.gmail.com>
2023-02-02 21:55:11 -0600

    Added gap between posts

--- src/features/social_media_posts_system/social_media_posts/social-media-posts.tsx
+++ src/features/social_media_posts_system/social_media_posts/social-media-posts.tsx
@@ -1,16 +1,13 @@
 import { FC, useContext, useEffect } from "react"
+import { Stack } from "react-bootstrap"
 import { SocialMediaPostsContext } from "../contexts/social-media-posts.context"
 import SocialMediaPost from "./social_media_post/social-media-post"
 
 const SocialMediaPosts: FC = () => {
   const { posts } = useContext(SocialMediaPostsContext)
 
-  useEffect(() => {
-    console.log(posts)
-  }, [posts])
-
   return (
-    <>
+    <Stack gap={3}>
       {posts.map(({ id, content, username, date_created }) => (
         <SocialMediaPost
           key={id}
@@ -19,7 +16,7 @@ const SocialMediaPosts: FC = () => {
           date_created={date_created}
         />
       ))}
-    </>
+    </Stack>
   )
 }
 
