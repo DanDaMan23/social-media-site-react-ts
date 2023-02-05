ccf340fa128e64503a0634a7db6172502bb9c1d6
DanDaMan23 <danlawrcons.gmail.com>
2023-02-05 17:53:58 -0600

    Connected post API call to create posts

--- src/features/social_media_posts_system/contexts/social-media-posts.context.tsx
+++ src/features/social_media_posts_system/contexts/social-media-posts.context.tsx
@@ -7,20 +7,27 @@ import {
   useState
 } from "react"
 import { AuthenticationContext } from "../../login_feature/contexts/authentication.context"
+import ICreateSocialMedialPostFormFields from "../create_social_media_post/form/social_media_post_form_fields.interface"
 import PostModel from "../models/post.model"
 
 interface ISocialMediaPostsContext {
   posts: PostModel[]
   error: string | null
   getNextPost: () => void
+  createNewPost: (formFields: ICreateSocialMedialPostFormFields) => void
   hasMorePosts: boolean
+  createPostSuccess: string | null
+  createPostError: string | null
 }
 
 export const SocialMediaPostsContext = createContext<ISocialMediaPostsContext>({
   posts: [],
   error: null,
   getNextPost: () => {},
-  hasMorePosts: true
+  createNewPost: () => {},
+  hasMorePosts: true,
+  createPostSuccess: null,
+  createPostError: null
 })
 
 const SocialMediaPostsContextProvider: FC<{ children: ReactNode }> = ({
@@ -29,6 +36,11 @@ const SocialMediaPostsContextProvider: FC<{ children: ReactNode }> = ({
   const [posts, setPosts] = useState<PostModel[]>([])
   const [nextPostsLink, setNextPostsLink] = useState<string | null>(null)
   const [error, setError] = useState<string | null>(null)
+
+  const [createPostSuccess, setCreatePostSuccess] = useState<string | null>(
+    null
+  )
+  const [createPostError, setCreatePostError] = useState<string | null>(null)
 
   const { token } = useContext(AuthenticationContext)
 
@@ -40,7 +52,7 @@ const SocialMediaPostsContextProvider: FC<{ children: ReactNode }> = ({
           method: "GET",
           headers: {
             "Content-Type": "application/json",
-            Authentication: `Token ${token}`
+            Authorization: `Token ${token}`
           }
         })
         const jsonResponse = await response.json()
@@ -61,7 +73,7 @@ const SocialMediaPostsContextProvider: FC<{ children: ReactNode }> = ({
           method: "GET",
           headers: {
             "Content-Type": "application/json",
-            Authentication: `Token ${token}`
+            Authorization: `Token ${token}`
           }
         })
         const jsonResponse = await response.json()
@@ -73,11 +85,35 @@ const SocialMediaPostsContextProvider: FC<{ children: ReactNode }> = ({
     }
   }
 
+  const createNewPost = async (
+    formFields: ICreateSocialMedialPostFormFields
+  ) => {
+    try {
+      const response = await fetch("/posts/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Token ${token}`
+        },
+        body: JSON.stringify({
+          content: formFields.postContent
+        })
+      })
+      const jsonResponse = await response.json()
+      setCreatePostSuccess(jsonResponse)
+    } catch (e) {
+      setCreatePostError((e as Error).message)
+    }
+  }
+
   const contextValue: ISocialMediaPostsContext = {
     posts,
     error,
     getNextPost,
-    hasMorePosts: !!nextPostsLink
+    createNewPost,
+    hasMorePosts: !!nextPostsLink,
+    createPostSuccess,
+    createPostError
   }
 
   return (
--- src/features/social_media_posts_system/create_social_media_post/modal/create_social_media_post_modal.tsx
+++ src/features/social_media_posts_system/create_social_media_post/modal/create_social_media_post_modal.tsx
@@ -1,6 +1,7 @@
-import { FC } from "react"
+import { FC, useContext } from "react"
 import { Button, Modal } from "react-bootstrap"
 import { SubmitHandler, useForm } from "react-hook-form"
+import { SocialMediaPostsContext } from "../../contexts/social-media-posts.context"
 import CreateSocialMedialPostForm from "../form/create_social_media_post_form"
 import ICreateSocialMedialPostFormFields from "../form/social_media_post_form_fields.interface"
 
@@ -13,13 +14,17 @@ const CreateSocialMedialPostModal: FC<ICreateSocialMedialPostModal> = ({
   show,
   onClose
 }) => {
+  const { createNewPost } = useContext(SocialMediaPostsContext)
+
   const {
     register,
     handleSubmit,
     formState: { isValid }
   } = useForm<ICreateSocialMedialPostFormFields>()
-  const onSubmit: SubmitHandler<ICreateSocialMedialPostFormFields> = (data) =>
-    console.log(data)
+  const onSubmit: SubmitHandler<ICreateSocialMedialPostFormFields> = (data) => {
+    // console.log(data)
+    createNewPost(data)
+  }
 
   return (
     <Modal show={show} onHide={onClose}>
