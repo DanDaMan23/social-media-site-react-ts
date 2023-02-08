2f5b579810fad69402f497676d0c0faacdcfb260
DanDaMan23 <danlawrcons.gmail.com>
2023-02-07 23:24:21 -0600

    Created apiCallWrapper that have spinner

--- src/features/api_calls_context/api_calls_context.tsx
+++ src/features/api_calls_context/api_calls_context.tsx
@@ -1,13 +1,18 @@
-import { createContext, FC, ReactNode, useContext } from "react"
+import { createContext, FC, ReactNode, useContext, useState } from "react"
 import OverlaySpinner from "../../components/overlay_spinner/overlay_spinner"
 import { AuthenticationContext } from "../login_feature/contexts/authentication.context"
 
 interface IAPICallsContext {
+  apiCallWrapper: (
+    handlerFunction: () => void,
+    errorHandlerFunction: (e: Error) => void
+  ) => void
   get: (link: string) => Promise<globalThis.Response>
   post: (link: string, body: {}) => Promise<globalThis.Response>
 }
 
 export const APICallsContext = createContext<IAPICallsContext>({
+  apiCallWrapper: () => {},
   get: () => new Promise<globalThis.Response>(() => {}),
   post: () => new Promise<globalThis.Response>(() => {})
 })
@@ -16,9 +21,26 @@ export const APICallsContextProvider: FC<{ children: ReactNode }> = ({
   children
 }) => {
   const { token } = useContext(AuthenticationContext)
+  const [showSpinner, setShowSpinner] = useState<boolean>(false)
+
   const headers = {
     "Content-Type": "application/json",
     Authorization: `Token ${token}`
+  }
+
+  const apiCallWrapper = (
+    handlerFunction: () => void,
+    errorHandlerFunction: (e: Error) => void
+  ) => {
+    setShowSpinner(true)
+
+    try {
+      handlerFunction()
+    } catch (e) {
+      errorHandlerFunction(e as Error)
+    }
+
+    setShowSpinner(false)
   }
 
   const get = async (link: string) =>
@@ -35,13 +57,14 @@ export const APICallsContextProvider: FC<{ children: ReactNode }> = ({
     })
 
   const contextValue: IAPICallsContext = {
+    apiCallWrapper,
     get,
     post
   }
 
   return (
     <APICallsContext.Provider value={contextValue}>
-      {/* <OverlaySpinner /> */}
+      {showSpinner && <OverlaySpinner />}
       {children}
     </APICallsContext.Provider>
   )
