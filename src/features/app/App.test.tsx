8b8a0dbd0739a677559f649f28dd03417df48347
DanDaMan23 <danlawrcons.gmail.com>
2023-02-19 18:32:12 -0600

    Setup and created tests

--- src/features/app/App.test.tsx
+++ src/features/app/App.test.tsx
@@ -1,9 +1,13 @@
-import React from "react"
-import { render, screen } from "@testing-library/react"
+import { render } from "@testing-library/react"
 import App from "./App"
+import { BrowserRouter } from "react-router-dom"
 
-test("renders learn react link", () => {
-  render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+test("render component correctly", () => {
+  const { container } = render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  )
+
+  expect(container).toMatchSnapshot()
 })
