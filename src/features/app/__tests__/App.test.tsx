09d090633dd1416ce5f2dc478a7aa452ff208532
DanDaMan23 <danlawrcons.gmail.com>
2023-02-19 18:37:38 -0600

    Created directory specifically for tests

--- src/features/app/__tests__/App.test.tsx
+++ src/features/app/__tests__/App.test.tsx
@@ -1,5 +1,5 @@
 import { render } from "@testing-library/react"
-import App from "./App"
+import App from "../App"
 import { BrowserRouter } from "react-router-dom"
 
 test("render component correctly", () => {
